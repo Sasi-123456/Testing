7a901d3d9df12e087f664a917796cb6285ced0fb
Sasi-123456 <168091844+Sasi-123456@users.noreply.github.com>
2024-04-25 17:25:44 +0530

    Update Do.java
    add object

--- Do.java
+++ Do.java
@@ -17,11 +17,15 @@ public class Do {
  Student s2 = new Student (55, "ram");
  Student s3 = new Student (55,"sai");
  Student s4 = new Student (152,"ram");
+ Student s5 = new Student (12,"raam");
+		
  
  s1.Display();
  s2.Display();
  s3.Display();
  s4.Display();
+ s5.Display();
+		
  
 		
 	}
