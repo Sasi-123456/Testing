e073c75826704c226646c36a4b763373d46fcd23
Sasi-123456 <168091844+Sasi-123456@users.noreply.github.com>
2024-04-25 17:22:59 +0530

    Do.java
    Example for java

---
+++ Do.java
@@ -0,0 +1,30 @@
+public class Do {
+	
+	int  id;
+	String name;
+	
+	Student(int i, String n){
+		id = i;
+		name = n;
+	}
+	void Display() {
+
+	System.out.println(id+""+name);
+	}
+	
+	public static void main(String[] args) {
+ Student s1 = new Student (111, " sasi");
+ Student s2 = new Student (55, "ram");
+ Student s3 = new Student (55,"sai");
+ Student s4 = new Student (152,"ram");
+ 
+ s1.Display();
+ s2.Display();
+ s3.Display();
+ s4.Display();
+ 
+		
+	}
+
+}
+
